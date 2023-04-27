0ff3cdeda3295e0302f8a976f4361765801fa8ab
spiffmeister <robospiff@gmail.com>
2023-04-28 00:31:13 +1000

    - Added code to NIMROD benchmark - Added code to pyocsim.jl

--- NIMROD_Benchmark.jl
+++ NIMROD_Benchmark.jl
@@ -2,12 +2,9 @@ using LinearAlgebra
 
 cd("..")
 using Interpolations
-push!(LOAD_PATH,"./plas_diff")
 push!(LOAD_PATH,"./SBP_operators")
 using SBP_operators
-using plas_diff
 
-using DifferentialEquations
 
 
 
@@ -16,15 +13,16 @@ using DifferentialEquations
 
 
 # Source term
-F(x,y) = -2π^2*cos(π*x)*cos(π*y)
+F(x,y,t) = 2π^2*cos(π*x)*cos(π*y)
 
 # Domain
 𝒟x = [-0.5,0.5]
 𝒟y = [-0.5,0.5]
-nx = ny = 32
+nx = ny = 16
 
 Dom = Grid2D(𝒟x,𝒟y,nx,ny)
 
+order = 2
 
 # Homogeneous boundary conditions
 BoundaryLeft    = Boundary(Dirichlet,(y,t) -> 0.0, Left, 1)
@@ -34,17 +32,17 @@ BoundaryDown    = Boundary(Dirichlet,(x,t) -> 0.0, Down, 2)
 
 
 # Initial condition
-u₀(x,y) = 0.0
+u₀(x,y) = cos(π*x)*cos(π*y)
 
 # Perpendicular diffusion coefficient
 k(x,y) = 1.0
 
 # Build PDE problem
-P = VariableCoefficientPDE2D(u₀,k,k,2,BoundaryLeft,BoundaryRight,BoundaryUp,BoundaryDown)
+P = VariableCoefficientPDE2D(u₀,k,k,order,BoundaryLeft,BoundaryRight,BoundaryUp,BoundaryDown)
 
 # Time domain
-Δt = 1.0Dom.Δx^2
-t_f = Inf
+Δt = 0.1Dom.Δx^2
+t_f = 1.0
 
 
 
@@ -52,22 +50,31 @@ t_f = Inf
 #===
     CONSTRUCT GRID
 ===#
-B(x,y,z) = [π*cos(π*x)*sin(π*y),
-    π*sin(π*x)*cos(π*y),
-    0]
-
+function B(X,x,p,t)
+    X[1] = π*cos(π*x[1])*sin(π*x[2])
+    X[2] = -π*sin(π*x[1])*cos(π*x[2])
+    # X[3] = 0.0
+end
 
 
     
-gdata = plas_diff.construct_grid(𝒟x,𝒟y,nx,ny,B)
+gdata   = construct_grid(B,Dom,[-2π,2π],ymode=:stop)
+Pfn     = generate_parallel_penalty(gdata,Dom,order)
 
+
+planeerror = zeros(nx,ny);
+for i = 1:nx
+    for j = 1:ny
+        planeerror[i,j] = norm([gdata.Fplane[i,j][1] - gdata.plane[i,j][1], gdata.Fplane[i,j][1] - gdata.plane[i,j][1]])
+    end
+end
 
 
 
 
 
 # Solve
-soln = solve(P,Dom,Δt,t_f,:cgie,adaptive=true)
+@time soln = solve(P,Dom,Δt,t_f,:cgie,adaptive=false,penalty_func=Pfn,source=F)
 
 
 # Exact solution
@@ -76,8 +83,17 @@ T(x,y,t) = (1.0 - exp(-2.0*k(x,y)*π^2*t) )/( k(x,y) )*Ψ(x,y)
 T_exact = zeros(Dom.nx,Dom.ny)
 for j = 1:ny
     for i = 1:nx
-        T_exact[i,j] = T(Dom.gridx[i],Dom.gridy[j],soln.t)
+        T_exact[i,j] = T(Dom.gridx[i],Dom.gridy[j],t_f)
     end
 end
+
+
+
+
+norm(T_exact - soln.u[2])/norm(T_exact)
+
+using Plots
+surface(T_exact)
+surface(soln.u[2])
 
 
--- pyocsim.jl
+++ pyocsim.jl
@@ -2,10 +2,8 @@ using LinearAlgebra
 
 cd("..")
 using Interpolations
-push!(LOAD_PATH,"./plas_diff")
 push!(LOAD_PATH,"./SBP_operators")
 using SBP_operators
-using plas_diff
 
 
 
@@ -26,14 +24,53 @@ params = (ϵₘₙ=[χₘₙ/2., χₘₙ/3.],m=[2.0, 3.0],n=[1.0, 2.0])
 function χ_h!(χ,x::Array{Float64},p,t)
     # Hamiltons equations for the field-line Hamiltonian
     # H = ψ²/2 - ∑ₘₙ ϵₘₙ(cos(mθ - nζ))
+    χ[2] = x[1] #p_1            qdot        θ
+    χ[1] = -sum(p.ϵₘₙ .*(sin.(p.m*x[2] - p.n*t) .* p.m)) #q_1        pdot        ψ
+    # χ[1] = x[2] #p_1            qdot        θ
+    # χ[2] = -sum(p.ϵₘₙ .*(sin.(p.m*x[1] - p.n*t) .* p.m)) #q_1        pdot        ψ
+end
+
+χ(χ,x,p,t) = χ_h!(χ,x,params,t)
+
+
+Pgrid   = construct_grid(χ,Dom,[-2π,2π],ymode=:stop)
+
+pfn     = generate_parallel_penalty(Pgrid,Dom,2)
+
+
+kx(x,y) = 1.0
+ky(x,y) = 1.0
+
+u₀(x,y) = x
+
+BoundaryLeft    = Boundary(Dirichlet,(y,t) -> 0.0,Left,1)
+BoundaryRight   = Boundary(Dirichlet,(y,t) -> 0.0,Right,1)
+BoundaryUpDown  = PeriodicBoundary(2)
+
+P = VariableCoefficientPDE2D(u₀,kx,ky,2,BoundaryLeft,BoundaryRight,BoundaryUpDown)
+
+
+t_f = 1.0
+Δt = Dom.Δy^2/100
+
+soln = solve(P,Dom,Δt,5.1Δt,:cgie,penalty_func=pfn)
+
+
+#=
+χₘₙ = 2.1e-3 + 5.0e-3
+params = plas_diff.SampleFields.H_params([χₘₙ/2., χₘₙ/3.],[2.0, 3.0],[1.0, 2.0])
+
+function χ_h!(χ,x::Array{Float64},p,t)
+    # Hamiltons equations for the field-line Hamiltonian
+    # H = ψ²/2 - ∑ₘₙ ϵₘₙ(cos(mθ - nζ))
     χ[1] = x[2] #p_1            qdot        θ
     χ[2] = -sum(p.ϵₘₙ .*(sin.(p.m*x[1] - p.n*t) .* p.m)) #q_1        pdot        ψ
 end
 
-χ(χ,x,t) = χ_h(χ,x,params,t)
+gdata = plas_diff.construct_grid(𝒟x,𝒟y,nx,ny,χ_h!,params)
 
 
-SBP_operators.Parallel.construct_grid(χ,Dom,[-2π,2π])
-
-
-
+gdata.z_planes[1].x
+# gdata.z_planes[1].y
+Pgrid.Fplane
+=#
