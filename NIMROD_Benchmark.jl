f303105a300da0592432c34c3815c3b35eaf470e
spiffmeister <robospiff@gmail.com>
2023-05-01 11:24:40 +1000

    Updated NIMROD benchmark

--- NIMROD_Benchmark.jl
+++ NIMROD_Benchmark.jl
@@ -17,30 +17,30 @@ F(x,y,t) = 2π^2*cos(π*x)*cos(π*y)
 # Domain
 𝒟x = [-0.5,0.5]
 𝒟y = [-0.5,0.5]
-nx = ny = 32
+nx = ny = 33
 
 Dom = Grid2D(𝒟x,𝒟y,nx,ny)
 
 order = 2
 
 # Homogeneous boundary conditions
-BoundaryLeft    = Boundary(Dirichlet,(y,t) -> 0.0, Left, 1)
-BoundaryRight   = Boundary(Dirichlet,(y,t) -> 0.0, Right, 1)
-BoundaryUp      = Boundary(Dirichlet,(x,t) -> 0.0, Up, 2)
-BoundaryDown    = Boundary(Dirichlet,(x,t) -> 0.0, Down, 2)
+BoundaryLeft    = Boundary(Dirichlet,(y,t) -> cos(0.5π)*cos(π*y)    , Left, 1)
+BoundaryRight   = Boundary(Dirichlet,(y,t) -> cos(-0.5π)*cos(π*y)   , Right, 1)
+BoundaryUp      = Boundary(Dirichlet,(x,t) -> cos(π*x)*cos(0.5π)    , Up, 2)
+BoundaryDown    = Boundary(Dirichlet,(x,t) -> cos(π*x)*cos(-0.5π)   , Down, 2)
 
 
 # Initial condition
 u₀(x,y) = cos(π*x)*cos(π*y)
 
 # Perpendicular diffusion coefficient
-k(x,y) = 1.0e-2
+k(x,y) = 1.0
 
 # Build PDE problem
 P = VariableCoefficientPDE2D(u₀,k,k,order,BoundaryLeft,BoundaryRight,BoundaryUp,BoundaryDown)
 
 # Time domain
-Δt = 0.01Dom.Δx^2
+Δt = 0.1Dom.Δx^2
 t_f = 10.0
 
 
@@ -55,31 +55,13 @@ function B(X,x,p,t)
     # X[3] = 0.0
 end
 
-# B(x) = Point2f([-π*cos(π*x[1])*sin(π*x[2])
-# π*sin(π*x[1])*cos(π*x[2])
-# ])
-
-# P = ODEProblem(B,[0.2,0.0],(0.0,2π))
-# solve(P,Rodas4P())
-
-gdata   = construct_grid(B,Dom,[-2π,2π],ymode=:stop)
+gdata   = construct_grid(B,Dom,[-2.0,2.0],ymode=:stop)
 Pfn     = generate_parallel_penalty(gdata,Dom,order)
 
 
-planeerror = zeros(nx,ny);
-for i = 1:nx
-    for j = 1:ny
-        planeerror[i,j] = norm([gdata.Fplane[i,j][1] - gdata.plane[i,j][1], gdata.Fplane[i,j][1] - gdata.plane[i,j][1]])
-    end
-end
-
-
-
-
-
 # Solve
-@time soln = solve(P,Dom,Δt,2.1Δt,:cgie,adaptive=true,penalty_func=Pfn,source=F)
-@time soln = solve(P,Dom,Δt,t_f,:cgie,adaptive=true,penalty_func=Pfn,source=F)
+@time soln = solve(P,Dom,Δt,2.1Δt,:cgie,adaptive=false,Pgrid=gdata,source=F)
+@time soln = solve(P,Dom,Δt,t_f,:cgie,adaptive=false,Pgrid=gdata,source=F)
 
 
 # Exact solution
@@ -93,12 +75,31 @@ for j = 1:ny
 end
 
 
+println(T(0.0,0.0,t_f) .- soln.u[2][argmin(abs.(Dom.gridx)),argmin(abs.(Dom.gridy))])
 
+println(soln.u[2][argmin(abs.(Dom.gridx)),argmin(abs.(Dom.gridy))] .- k(0.0,0.0))
 
-norm(T_exact - soln.u[2])/norm(T_exact)
 
 using Plots
 surface(T_exact)
 surface(soln.u[2])
 
 
+
+
+
+
+#=
+
+ϵ = zeros(nruns)
+for i = 1:nruns
+    ϵ[i] = norm( 1/k(0,0) - solns[i].soln.u[2][ argmin(abs.(solns[i].Dom.gridx)), argmin(abs.(solns[i].Dom.gridy)) ] )
+end
+
+using DelimitedFiles
+nameappend=string("k=",k(0,0))
+open(string("NIMROD_benchmark",nameappend,".csv"),"w") do io
+    writedlm(io,[N ϵ])
+end
+
+=#
